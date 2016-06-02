2043387c2c71c81962bd09da1d4688e70ea3b0e8
Shawn Poindexter <shawnpoindexter@gmail.com>
2016-06-01 21:16:30 -0700

    wip

--- gridsearch.py
+++ gridsearch.py
@@ -3,19 +3,21 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 
 class TestCheckin(BaseEstimator, ClassifierMixin):
 
-    cache = {}
+    cache = None
 
     def __init__(self, a, b):
-        if 'a' in self.cache.keys() and self.cache['a'] == a:
-            print("init - rerun")
-        else:
+        print("cache in init",self.cache)
+        if self.cache is None:
             print("init")
+        else:
+            print("init - rerun")
         self.a = a
         self.b = b
-        self.cache['a'] = a
 
     def fit(self, X, y):
         print("fit",self.a,self.b,X,y)
+        # print("cache",self.cache)
+        self.cache = { 'b': 2 }
         return self
 
     def score(self, X, y, sample_weight=None):
@@ -25,5 +27,8 @@ class TestCheckin(BaseEstimator, ClassifierMixin):
 from sklearn import grid_search
 
 clf = TestCheckin(a=1, b=2)
-search = grid_search.GridSearchCV(clf, { 'a': [5, 2, 3]}, refit=False)
-search.fit_predict([1, 2, 3],[4,5,6])
+clf.fit([1,2,3],[4,5,6])
+clf = TestCheckin(a=1, b=2)
+clf.fit([1,2,3],[4,5,6])
+# search = grid_search.GridSearchCV(clf, { 'a': [5, 2, 3]}, refit=False)
+# search.fit([1, 2, 3],[4,5,6])
