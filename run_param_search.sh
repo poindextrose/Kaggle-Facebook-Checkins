69b2befcb81b2c73049a3d62a31d4b0e7b55a6f2
Shawn Poindexter <shawnpoindexter@gmail.com>
2016-06-07 21:36:47 -0700

    fix typo

--- run_param_search.sh
+++ run_param_search.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python param_search.py a_scale="gmmma(a=1,scale=1)" a_bias="expon(scale=200)" a_min="expon(0,20)" \
+python param_search.py a_scale="gamma(a=1,scale=1)" a_bias="expon(scale=200)" a_min="expon(0,20)" \
 day_hist_bins="randint(1,24)" day_hist_min_prob="uniform(0,0.9)" \
 week_hist_bins="randint(1,24)" week_hist_min_prob="uniform(0,0.9)" \
 year_hist_bins="randint(1,24)" year_hist_min_prob="uniform(0,0.9)" \
